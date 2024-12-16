348fb11d55fef084c6a8e93bd2685ec12dbe2ef5
Anastasia Ivankina <anst.iv@yandex.ru>
2024-12-16 17:51:40 +0300

    Тесты. Проверка 2

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,22 +1,31 @@
 /// <reference types='cypress' />
 
+const SELECTORS = {
+  BUN_INGREDIENT: '[data-cy="bun-ingredient"]',
+  MAINS_INGREDIENT: '[data-cy="mains-ingredient"]',
+  CONSTRUCTOR_INGREDIENTS: '[data-cy="constructor-indredients"]',
+  BUTTON_CLOSE: '#modals button[aria-label="Закрыть"]',
+  ORDER_SUM: '[data-cy="order-sum"]',
+  ORDER_NUMBER: '[data-cy="order-number"]'
+}
+
 describe('Добавление ингредиента в конструктор', function () {
   beforeEach(() => {
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json'});
     cy.viewport(1300, 800);
-    cy.visit('http://localhost:4000');
+    cy.visit('');
   });
 
   it('Добавление булки в конструктор', () => {
-    cy.get('[data-cy="bun-ingredient"]').contains('Добавить').click();
+    cy.get(SELECTORS.BUN_INGREDIENT).contains('Добавить').click();
     cy.get('[data-cy="constructor-bun-1"]')
       .contains('Булочка с кунжутом')
       .should('exist')
   });
 
   it('Добавление ингредиента в конструктор', () => {
-    cy.get('[data-cy="mains-ingredient"]').contains('Добавить').click();
-    cy.get('[data-cy="constructor-indredients"]')
+    cy.get(SELECTORS.MAINS_INGREDIENT).contains('Добавить').click();
+    cy.get(SELECTORS.CONSTRUCTOR_INGREDIENTS)
       .contains('Говяжья котлета')
       .should('exist')
   });
@@ -26,7 +35,7 @@ describe('Корректная работа модальных окон', functi
   beforeEach(() => {
     cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json'});
     cy.viewport(1300, 800);
-    cy.visit('http://localhost:4000');
+    cy.visit('');
   });
 
   it('Открытие модального окна', () => {
@@ -39,7 +48,7 @@ describe('Корректная работа модальных окон', functi
   it('Закрытие модального окна по кнопке', () => {
     cy.contains('Говяжья котлета').click();
     cy.contains('Детали ингредиента').should('exist');
-    cy.get('#modals button[aria-label="Закрыть"]').click();
+    cy.get(SELECTORS.BUTTON_CLOSE).click();
     cy.contains('Детали ингредиента').should('not.exist');
   });
 
@@ -64,7 +73,7 @@ describe('Создание заказа', function () {
     );
     cy.setCookie('accessToken', 'test1')
     cy.viewport(1300, 800);
-    cy.visit('http://localhost:4000');
+    cy.visit('');
   });
 
   afterEach(() => {
@@ -73,11 +82,11 @@ describe('Создание заказа', function () {
   });
 
   it('Оформление заказа', () => {
-    cy.get('[data-cy="bun-ingredient"]').contains('Добавить').click();
-    cy.get('[data-cy="mains-ingredient"]').contains('Добавить').click();
+    cy.get(SELECTORS.BUN_INGREDIENT).contains('Добавить').click();
+    cy.get(SELECTORS.MAINS_INGREDIENT).contains('Добавить').click();
     cy.get('[data-cy="sauces-ingredient"]').contains('Добавить').click();
-    cy.get('[data-cy="order-sum"]').click()
-    cy.get('[data-cy="order-sum"]').click()
+    cy.get(SELECTORS.ORDER_SUM).click()
+    cy.get(SELECTORS.ORDER_SUM).click()
     
     cy.wait('@postOrder')
     .its('request.body')
@@ -85,18 +94,18 @@ describe('Создание заказа', function () {
       ingredients: ['1', '5', '2', '2']
     });
     
-    cy.get('[data-cy="order-number"]').contains('123');
+    cy.get(SELECTORS.ORDER_NUMBER).contains('123');
 
-    cy.get('#modals button[aria-label="Закрыть"]').click();
-    cy.contains('[data-cy="order-number"').should('not.exist');
+    cy.get(SELECTORS.BUTTON_CLOSE).click();
+    cy.contains(SELECTORS.ORDER_NUMBER).should('not.exist');
 
-    cy.get('[data-cy="constructor-indredients"]')
+    cy.get(SELECTORS.CONSTRUCTOR_INGREDIENTS)
       .contains('Булочка с кунжутом')
       .should('not.exist');
-    cy.get('[data-cy="constructor-indredients"]')
+    cy.get(SELECTORS.CONSTRUCTOR_INGREDIENTS)
       .contains('Говяжья котлета')
       .should('not.exist');
-    cy.get('[data-cy="constructor-indredients"]')
+    cy.get(SELECTORS.CONSTRUCTOR_INGREDIENTS)
       .contains('Соус барбекю')
       .should('not.exist')
   });
